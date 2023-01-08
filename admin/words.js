b9d182080964ba8e6970d4cb371194c252d8e8d8
Christian <CDuernberger8463@cosmoconsult.com>
2023-01-08 19:35:55 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'smartstate adapter settings': {
+        'en': 'Adapter settings for smartstate',
+        'de': 'Adaptereinstellungen für smartstate',
+        'ru': 'Настройки адаптера для smartstate',
+        'pt': 'Configurações do adaptador para smartstate',
+        'nl': 'Adapterinstellingen voor smartstate',
+        'fr': "Paramètres d'adaptateur pour smartstate",
+        'it': "Impostazioni dell'adattatore per smartstate",
+        'es': 'Ajustes del adaptador para smartstate',
+        'pl': 'Ustawienia adaptera dla smartstate',
+        'zh-cn': 'smartstate的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
