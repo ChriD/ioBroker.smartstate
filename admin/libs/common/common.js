bbb8bc9b0e4318e5c0e5a447c335324883d34a49
Christian <CDuernberger8463@cosmoconsult.com>
2023-01-20 22:37:55 +0100

    initial

--- admin/libs/common/common.js
+++ admin/libs/common/common.js
@@ -17,3 +17,54 @@ function formatObjectId(_value)
 
     return validObjectId;
 }
+
+// this function will get the row index for an element which is within the child state grid or for the row 
+// element itself
+function getTableRowDataIndex(_childOrRowElement)
+{
+    let rowElement = _childOrRowElement.tagName instanceof HTMLTableRowElement ? _childOrRowElement : _childOrRowElement.closest('tr');
+    if(rowElement)
+        return rowElement.getAttribute('data-index');
+    return '';
+}
+
+
+function GUI2Object(_object)
+{
+    $('.value').each(function () {
+        var $this = $(this);
+        
+        if ($this.attr('type') === 'checkbox') {
+            _object[$this.attr('id')] = $this.prop('checked');
+        } else if ($this.attr('type') === 'number') {
+            _object[$this.attr('id')] = parseFloat($this.val());
+        } else {
+            _object[$this.attr('id')] = $this.val();
+        }
+    });            
+}
+
+
+function object2GUI(_object)
+{
+    $('.value').each(function () {
+        var $key = $(this);
+        var id = $key.attr('id');
+
+        if($key.is('select'))
+        {                                 
+            $key.val(_object[id]).select();                  
+        }
+        else if ($key.attr('type') === 'checkbox') 
+        {
+            // do not call onChange direct, because onChange could expect some arguments
+            $key.prop('checked', _object[id]);
+        }            
+        else
+        {
+            // do not call onChange direct, because onChange could expect some arguments
+            $key.val(_object[id]);
+        }
+    });
+    if (M) M.updateTextFields();
+}
